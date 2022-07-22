5a098c5572224f3aefdd5893667d7fde42ab2bc7
surajit barman <surajitbarman57@yahoo.in>
2022-07-22 13:28:53 +0530

    updated user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput): User {
-    return { exampleField: createUserInput.exampleField };
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  create(createUserInput: CreateUserInput) {
+    const user = new this.userModel(createUserInput);
+    return user.save();
   }
 
-  findAll(): [User] {
-    return [{ exampleField: 1 }];
+  findAll() {
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number): User {
-    return { exampleField: id };
+  async findOne(id: string) {
+    const user = await this.userModel.findOne({ _id: id }).exec();
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return user;
   }
 
-  update(id: number, updateUserInput: UpdateUserInput): User {
-    return { exampleField: updateUserInput.id };
+  async update(id: string, updateUserInput: UpdateUserInput) {
+    const existingUser = await this.userModel
+      .findOneAndUpdate({ _id: id }, { $set: updateUserInput }, { new: true })
+      .exec();
+
+    if (!existingUser) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return existingUser;
   }
 
-  remove(id: number): User {
-    return { exampleField: id };
+  async remove(id: string) {
+    const user = await this.findOne(id);
+    return user.remove();
   }
 }
