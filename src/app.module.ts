fbbcfd5be5ed0f7249eadb2a6cf02e51f18463db
surajit barman <surajitbarman57@yahoo.in>
2022-07-22 13:20:22 +0530

    added common module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CommonModule } from './common/common.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    CommonModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
