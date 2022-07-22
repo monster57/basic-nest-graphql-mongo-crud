bd1dbd039f4224ddcbd93124aa46f19c14e5a38f
surajit barman <surajitbarman57@yahoo.in>
2022-07-22 13:19:36 +0530

    created common module for graphql and confige module

---
+++ src/common/common.module.ts
@@ -0,0 +1,11 @@
+
+
+import { Module } from '@nestjs/common';
+import { GraphqlModule } from './graphql.module';
+import { ConfigModule } from './config.module';
+
+@Module({
+  imports: [ConfigModule, GraphqlModule],
+  exports: [ConfigModule, GraphqlModule],
+})
+export class CommonModule {}
